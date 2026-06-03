34cfd86c8557c9ae90d520f77386ed39acf06eed
EdisonSA <esilo0924@gmail.com>
2026-06-03 17:03:04 -0500

    Refactor: Renamed application from gestion_bar to gestion_rosa_nieve

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from gestion_bar import views  
+from gestion_rosa_nieve import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
